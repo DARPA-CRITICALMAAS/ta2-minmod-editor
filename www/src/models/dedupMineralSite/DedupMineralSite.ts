dd151726c445f2e990bb219c26f3e67e8d59245d
NithyashreeGS30 <143140576+NithyashreeGS30@users.noreply.github.com>
2024-12-12 00:53:18 -0800

    Added confidence user (#37) fixed #22
    Co-authored-by: Binh Vu <bvu687@gmail.com>

--- www/src/models/dedupMineralSite/DedupMineralSite.ts
+++ www/src/models/dedupMineralSite/DedupMineralSite.ts
@@ -42,6 +42,23 @@ export class DedupMineralSiteOriginalSite {
   }
 }
 
+export class DedupMineralSiteOriginalSite {
+  id: string;
+  score: number;
+
+  public constructor({ id, score }: { id: string; score: number }) {
+    this.id = id;
+    this.score = score;
+  }
+
+  public static deserialize(record: any): DedupMineralSiteOriginalSite {
+    return new DedupMineralSiteOriginalSite({
+      id: record.id,
+      score: record.score,
+    });
+  }
+}
+
 export class DedupMineralSiteLocation {
   lat?: number;
   lon?: number;
@@ -154,6 +171,20 @@ export class DedupMineralSite {
 
   public static getId(uri: string): string {
     return uri.substring(uri.lastIndexOf("/") + 1);
+  }
+
+  public static deserialize(record: any): DedupMineralSite {
+    return new DedupMineralSite({
+      id: record.id,
+      uri: `https://minmod.isi.edu/resource/${record.id}`,
+      name: record.name,
+      type: record.type,
+      rank: record.rank,
+      sites: record.sites.map((site: any) => DedupMineralSiteOriginalSite.deserialize(site)),
+      depositTypes: record.deposit_types.map((depositType: any) => new DedupMineralSiteDepositType(depositType)),
+      location: record.location !== undefined ? DedupMineralSiteLocation.deserialize(record.location) : undefined,
+      gradeTonnage: GradeTonnage.deserialize(record.grade_tonnage),
+    });
   }
 
   public getTop1DepositType(): DedupMineralSiteDepositType | undefined {
